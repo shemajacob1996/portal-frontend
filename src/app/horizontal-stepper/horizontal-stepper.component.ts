0b50d314e9340f0669b4cac45908f9c197d93cf3
shema <shema.jacob@perfomatix.com>
2022-06-08 13:05:36 +0530

    angular 12 updation and initialization

---
+++ src/app/horizontal-stepper/horizontal-stepper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-stepper',
+  templateUrl: './horizontal-stepper.component.html',
+  styleUrls: ['./horizontal-stepper.component.scss']
+})
+export class HorizontalStepperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
