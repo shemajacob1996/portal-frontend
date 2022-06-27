8f533a492779176553a1c307b6717cd290af246a
shema <shema.jacob@perfomatix.com>
2022-06-27 15:52:25 +0530

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { HttpService } from './http.service';
 import { IDataModel, IResponse } from './shared/model/data.model';
 import { UtilService } from './util.service';
@@ -12,18 +14,38 @@ export class AppComponent implements OnInit, OnDestroy {
   public rootMasterData !: IDataModel;
   title = 'portal-frontend';
 
-  constructor(private _utilService: UtilService, private _httpService: HttpService ) { }
+  constructor(private _utilService: UtilService,
+     private _httpService: HttpService, 
+     private activatedRoute: ActivatedRoute,
+     private router: Router, ) { }
+     refNum;
+     knEdit;
+     notFound :boolean = false;
   
   ngOnInit(): void {
     this._utilService.activeStepper.subscribe((res: any) => {
       document.getElementById(res)?.scrollIntoView();
     });
-
-    this.getMasterAPIData();
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(x => {
+      const referenceNumber = this.activatedRoute.snapshot.queryParams["ref-num"] || this.activatedRoute.snapshot.queryParams["token"];
+      this._httpService.refNum = referenceNumber;
+      this.refNum = referenceNumber;
+      this.knEdit = this.activatedRoute.snapshot.queryParams.mode;
+      // if (this.refNum === undefined) {
+      //   this.notFound = true;
+      //   this.router.navigate(["/404"]);
+      // } else {
+        this.getMasterAPIData();
+      // }
+    });
+    
   }
 
   getMasterAPIData(){
-    this._httpService.getMasterResponseData().subscribe((response: IResponse) => {
+    const isTokenMode = this.activatedRoute.snapshot.queryParams['token'] ? true : false;
+    this._httpService.getMasterResponseData(this.refNum,isTokenMode).subscribe((response: IResponse) => {
       this.rootMasterData = response?.data;
     });
   }
